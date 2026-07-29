241246757c36ce2e345f6c3dc9c2b630f67c52de
parkej1130-dev <parkej1130@gmail.com>
2026-07-29 11:13:17 +0900

    Add static app manifests

---
+++ docs/_next/static/M9g4Lpp9xYPxwu4ZJHoDN/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ev-system-lab//_next/:path+",
+        "destination": "/ev-system-lab/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
